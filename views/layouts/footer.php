b8ce81669ece9826dae5b7790ea27eea469cd236
MagistrZP <magistr-zp@mail.ru>
2017-02-02 13:12:43 +0200

    lesson 4

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -22,7 +22,7 @@
         $(".add-to-cart").click(function () {
             var id = $(this).attr("data-id");
             $.post("/cart/addAjax/" + id, {}, function (data) {
-                $("#cart-count").html(data);
+                $("#cart-count").html("("+data+")");
 
             });
             return false;
